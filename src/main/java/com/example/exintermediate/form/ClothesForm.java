02ff53eed1e17071f4a6b28ac4e53c3ea0cfc5d3
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-24 14:09:25 +0900

    ClothesForm.java作成

---
+++ src/main/java/com/example/exintermediate/form/ClothesForm.java
@@ -0,0 +1,19 @@
+package com.example.exintermediate.form;
+
+public class ClothesForm {
+  private String gender;
+  private String color;
+  
+  public String getGender() {
+    return gender;
+  }
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+}
